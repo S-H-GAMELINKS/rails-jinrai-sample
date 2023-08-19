7df51862c14daeb43b896cfa2b399dd9b8abb77e
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-08-19 15:55:24 +0900

    Add Posts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times do
+  Post.create!(titile: "Title:#{_1}", content: "Content:#{_1}")
+end
